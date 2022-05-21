6e621730d6abd8365a670e32275b8f883364e67a
parw8649 <cparwatkar@csumb.edu>
2022-05-21 09:16:13 +0530

    Testing fixes for db configuration

--- backend/getHistory.php
+++ backend/getHistory.php
@@ -2,21 +2,26 @@
 require '../results/telemetry_settings.php';
 require_once '../results/telemetry_db.php';
 
-ini_set('display_errors', 1);
-
 $fetchData= fetchSpeedTestHistory();
 
 show_data($fetchData);
 
 function fetchSpeedTestHistory() {
 
+
+    $dsn = 'mysql:'
+    .'host='.$MySql_hostname
+    .';port='.$MySql_port
+    .';dbname='.$MySql_databasename;
+
+    $test_pdo = new PDO($dsn, $MySql_username, $MySql_password, $pdoOptions);
+
+    if (!($test_pdo instanceof PDO)) {
+        echo "<tr><td colspan='10'>There was a PDO error.</td></tr>";
+        return false;
+    }
+
     try {
-        $test_pdo = getPdo();
-        if (!($test_pdo instanceof PDO)) {
-            echo "<tr><td colspan='10'>There was a PDO error.</td></tr>";
-            return false;
-        }
-    
         $stmt = $test_pdo->query(
                 'SELECT
                 id, timestamp, ip, ispinfo, ua, lang, dl, ul, ping, jitter, log, extra
@@ -38,8 +43,6 @@ function fetchSpeedTestHistory() {
     return $rows;
 }
 
-
-
 function show_data($fetchData) {
 
     if (false === $fetchData) {
@@ -48,30 +51,10 @@ function show_data($fetchData) {
     } elseif (empty($fetchData)) {
         echo '<tr><td colspan='10'>Could not find any speedtest results in database.</td></tr>';
         return;
-    }
-  
-    if(count($fetchData) > 0){
-     
-    $sn = 1;
-      foreach($fetchData as $data){ 
-        echo "<tr>
-                <td>"$sn"</td>
-                <td>"$data['timestamp']"</td>
-                <td>"$data['ip']"</td>
-                <td>"$data['ua']"</td>
-                <td>"$data['lang']"</td>
-                <td>"$data['dl']"</td>
-                <td>"$data['ul']"</td>
-                <td>"$data['ping']"</td>
-                <td>"$data['jitter']"</td>
-                <td>"$data['ispinfo']"</td>
-            </tr>";
-        $sn++;
-     }
-    } else {   
-        echo "<tr>
-                <td colspan='10'>No Data Found</td>
-            </tr>"; 
+    } else {
+        echo "<tr><td colspan='5'>$fetchData</td>
+        <td colspan='5'>$fetchData['dl']</td>
+        </tr>";
     }
 }
 ?>
